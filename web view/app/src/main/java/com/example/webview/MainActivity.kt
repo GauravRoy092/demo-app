8b949c60c95943612bfc4259dba6adeaf82a3860
Gaurav <royg48848@gmail.com>
2024-10-30 23:56:57 +0530

    web view

---
+++ web view/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.webview
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false) // Edge-to-edge support
+        setContentView(R.layout.activity_main)
+
+        val webViewVariable = findViewById<WebView>(R.id.webdekh)
+        webViewSetup(webViewVariable)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetup(webView: WebView) {
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://gauravroyy.netlify.app/")
+        }
+    }
+}
